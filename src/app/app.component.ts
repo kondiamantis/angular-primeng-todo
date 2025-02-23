48d5d4cfcb72a8cda420b77c7edf7dd6a9e9de39
kdiamantis <kdiamantis@iknowhow.com>
2025-02-23 19:29:19 +0200

    Basic todo list with hard coded info, onchange in checkbox doesn't work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,35 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { Todo } from './interfaces/todo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, CardModule],
+  imports: [RouterOutlet, ButtonModule, CardModule, TableModule, CheckboxModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-primeng-todo';
+  todos = [
+    { id: 1,
+      task: 'first todo',
+      completed: true
+    },
+    { id: 2,
+      task: 'second todo',
+      completed: false
+    }
+  ]
+
+  updateTodo(e: unknown, todo: Todo) {
+    console.log(e);
+    console.log(todo);
+  }
+
 }
+
+
